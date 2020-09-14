d64480bd35b19a5f2f5dc316a73bef66b8f042af
Kelvin Batista Machado <kelvinmachado@live.com>
2020-09-14 18:17:06 -0300

    Initial Commit

---
+++ MyGameList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyGameList
+//
+//  Created by Kelvin Batista Machado on 14/09/20.
+//  Copyright © 2020 Kelvin Batista Machado. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
