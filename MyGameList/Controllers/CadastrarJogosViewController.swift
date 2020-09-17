931ba7bf4e10d2ac3c91f8eadbb27b88d5ba2cd3
Kelvin Batista Machado <kelvinmachado@live.com>
2020-09-17 20:08:28 -0300

    Adiciona telas de Cadastro de Jogos e Meus Jogos

---
+++ MyGameList/Controllers/CadastrarJogosViewController.swift
@@ -0,0 +1,18 @@
+//
+//  CadastrarJogosViewController.swift
+//  MyGameList
+//
+//  Created by Kelvin Batista Machado on 17/09/20.
+//  Copyright © 2020 Kelvin Batista Machado. All rights reserved.
+//
+
+import UIKit
+
+class CadastrarJogosViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
---
+++ MyGameList/Controllers/MeusJogosViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MeusJogosViewController.swift
+//  MyGameList
+//
+//  Created by Kelvin Batista Machado on 17/09/20.
+//  Copyright © 2020 Kelvin Batista Machado. All rights reserved.
+//
+
+import UIKit
+
+class MeusJogosViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+
+
+}
